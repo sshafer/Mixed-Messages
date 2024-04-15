dd6b3009c64313f8cfcde89bfa25f65e71e4421b
Sshafer <sshafer@users.noreply.github.com>
2024-04-15 15:45:53 -0400

    filled output array, created messageOutput function

--- main.js
+++ main.js
@@ -3,20 +3,37 @@
 // Random number function
 function generateRandomNumber(num) {
     return Math.floor(Math.random()*num)
-
+};
 // Object of 3 arrays for messages
 const randomMessage = {
     names: [],
     verbs:[],
-    adverbs[],
+    adverbs:[],
 },
-
 // output array
 const message = [];
 
+//fill output array
+for (let value in randomMessage){
+    let choice = generateRandomNumber(randomMessage[choice].length)
+    
+    switch(value) {
+        case 'names':
+            message.push("$randomMessage[value][choice] ")
+            break
+        case 'verbs':
+            message.push("$randomMessage[value][choice] ")
+            break
+        case 'adverbs':
+            message.push("$randomMessage[value][choice].")
+            break
+
+    }
 }
 //create function to display output array
-  
-//fill output array
-
+function messageOutput(output){
+    const format = message.join
+    console.log(format)
+}
 //call output array function
+messageOutput(message)

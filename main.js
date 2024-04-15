77065ecd6dc1d17400bc61eefacde59803a5f2d7
Sshafer <sshafer@users.noreply.github.com>
2024-04-15 14:50:51 -0400

    addend generateRandomNumber function

--- main.js
+++ main.js
@@ -6,6 +6,11 @@
 //create output array
  
 //create random number function
-
+function generateRandomNumber(num) {
+    return Math.floor(Math.random()*num)
+}
 //create function to display output array
   
+//fill output array
+
+//call output array function

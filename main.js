06ad1c9e0db895c5c3f67337f08ba6c483e4682f
Sshafer <sshafer@users.noreply.github.com>
2024-04-15 14:01:21 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Display a three-part message
+    (define message type) */
+
+//create object of 3 arrays for messages
+
+//create output array
+ 
+//create random number function
+
+//create function to display output array
+  
